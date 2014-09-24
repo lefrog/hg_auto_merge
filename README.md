a3c26872b970030091fb03aa618667b78ed75fc1
Pascal Lambert <foo@bar.com>
2014-09-24 15:48:41 -0600

    fix a bunch of issues...

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 # hg_auto_merge.js
 
 Simple Node.js script to perform auto merge between 2 repositories. Typically a child repo pulling from it's parent's one.
-The script use environment variables to know current repo and parent repo.
-
-#### Environment Variables
-
-###### current_branch_name
-###### parent_branch_name
 
 #### Usage
 
-    node hg_auto_merge.js
+    node hg_auto_merge.js <parent repo url> [merge commit comment]
 
--- hg_auto_merge.js
+++ hg_auto_merge.js
@@ -1,7 +1,15 @@
+/**
+ * https://github.com/lefrog/hg_auto_merge
+ *
+ * node hg_auto_merge.js <parent repo url> [merge commit comment]
+ */
+
+var util = require('util');
 var exec = require('child_process').exec;
 
-var current_repo = process.env["current_branch_name"];
-var parent_repo = process.env["parent_branch_name"];
+var parent_repo = process.argv[2] || process.env['parent_branch_name'];
+var commit_message = process.argv[3] || util.format('Merge with %s', parent_repo);
+
 
 function doPush() {
     exec("hg push", function(err, stdout, stderr) {
@@ -15,13 +23,20 @@ function doPush() {
 }
 
 function doMerge() {
-    var msg = "Merge " + parent_repo + " into " + current_repo;
-    exec("hg merge -t internal:merge && hg commit -m '" + msg + "'", function(err, stdout, stderr) {
+    exec("hg merge -t internal:merge", function(err, stdout, stderr) {
         console.log(stdout);
         if (err) {
             console.log("Failed to merge: %s", stderr);
+            throw new Error("Failed to merge");
         } else {
-            doPush();
+            var cmd = util.format('hg commit -m "%s"', commit_message);
+            exec(cmd, function(err, stdout, stderr) {
+                if (err) {
+                    console.log("Failed to commit merge: %s", stderr);
+                } else {
+                    doPush();
+                }
+            });
         }
     });
 }
@@ -32,17 +47,23 @@ function doUpdate() {
         if (err) {
             console.log("Update failed: %s", stderr);
         } else {
-            doPush();
+            if (stdout.match(/no changes found/) == null) {
+                doPush();
+            }
         }
     });
 }
 
 function doDetectMultipleHeads() {
     exec("hg heads default", function(err, stdout, stderr) {
-        if (stdout.lastIndexOf("changeset") > 1) {
-            doMerge();
+        if (err) {
+            console.log('Failed to get heads: %s', err);
         } else {
-            doUpdate();
+            if (stdout.match(/changeset:   /g).length > 1) {
+                doMerge();
+            } else {
+                doUpdate();
+            }
         }
     });
 }
@@ -52,7 +73,6 @@ exec("hg pull " + parent_repo, function(err, stdout, stderr) {
     if (err) {
         console.log("Failed pull %s", stderr);
     } else {
-        console.log(stdout);
         doDetectMultipleHeads();
     }
 });
